178aa72ca7abf0bba7b15bd8f26b5e8db396988a
adambonneruk <adam@bonner.uk>
2021-09-05 19:28:52 +0100

    layout, added 'graph' and 'ss' functions

--- WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,18 @@
+# setup posh-git
 Import-Module posh-git
 Import-Module oh-my-posh
 Set-Theme Paradox
-remove-item alias:ls
+
+# remove default alias, to replaced by unix/custom code
+Remove-Item alias:ls
+Remove-Item alias:rm
+Remove-Item alias:curl
+
+# new alias
+#Set-Alias -Name graph -Value "git log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+
+function graph {git log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --all}
+function ss {git status --short}
+
+# remove any mess/clear the screen
 cls
