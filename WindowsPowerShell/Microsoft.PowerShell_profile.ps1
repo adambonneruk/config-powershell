1ead18286c66db35bacd980eeeea9133c44dfa52
adambonneruk <adam@bonner.uk>
2021-09-05 19:41:37 +0100

    added comments, fixed use of incorrect 'cls'

--- WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -3,16 +3,14 @@ Import-Module posh-git
 Import-Module oh-my-posh
 Set-Theme Paradox
 
-# remove default alias, to replaced by unix/custom code
+# remove default alias, to replaced by *nix / coreutils /custom functions
 Remove-Item alias:ls
 Remove-Item alias:rm
 Remove-Item alias:curl
 
-# new alias
-#Set-Alias -Name graph -Value "git log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
-
+# custom functions (for complex alias)
 function graph {git log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --all}
 function ss {git status --short}
 
 # remove any mess/clear the screen
-cls
+Clear-Host
