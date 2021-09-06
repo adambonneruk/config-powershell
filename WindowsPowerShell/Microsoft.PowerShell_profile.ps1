f0b7d4fcd5948685e3d04938ce46d38845f085e1
adambonneruk <adam@bonner.uk>
2021-09-06 16:22:15 +0100

    added additional work-context functions

--- WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -11,6 +11,9 @@ Remove-Item alias:curl
 # custom functions (for complex alias)
 function graph {git log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --all}
 function ss {git status --short}
+function nuke {git reset --hard; git clean -xdf}
+function tenmeg {openssl rand -out ./data.file -base64 7743330}
+function watch {while ($true) { forfiles /P . /S /C "CMD /C ECHO @FDATE @PATH" | Out-Host; Sleep 5; Clear }}
 
 # remove any mess/clear the screen
 Clear-Host
