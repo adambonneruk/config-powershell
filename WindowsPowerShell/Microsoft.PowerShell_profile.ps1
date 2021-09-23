28cbe30ea6b63f5540a94f41acdffbfec28295ae
adambonneruk <adam@bonner.uk>
2021-09-23 20:32:29 +0100

    added ll and improved watch (using python + alias)

--- WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -8,12 +8,15 @@ Remove-Item alias:ls
 Remove-Item alias:rm
 Remove-Item alias:curl
 
+# custom alias
+Set-Alias watch ~\github\watch\watch.py
+
 # custom functions (for complex alias)
 function graph {git log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --all}
 function ss {git status --short}
 function nuke {git reset --hard; git clean -xdf}
 function tenmeg {openssl rand -out ./data.file -base64 7743330}
-function watch {while ($true) { forfiles /P . /S /C "CMD /C ECHO @FDATE @PATH" | Out-Host; Sleep 5; Clear }}
+function ll {ls -lah}
 
 # remove any mess/clear the screen
 Clear-Host
